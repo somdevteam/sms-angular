b15ff5ceaea65b249349d5e70c4b8d17b2e436e0
Abdihakiim121 <cabdixakiin121@gmail.com>
2024-07-17 20:05:35 +0300

    payment fees

--- src/app/fees/fees-routing.module.ts
+++ src/app/fees/fees-routing.module.ts
@@ -1,10 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AuthGuard} from "@core/guard/auth.guard";
+import {AddUserComponent} from "../users/add-user/add-user.component";
+import {ListUsersComponent} from "../users/list-users/list-users.component";
+import {Permissions} from "@shared/enums/permissions.enums";
+import {AddfeesComponent} from "./addfees/addfees.component";
+import {AllfeesComponent} from "./allfees/allfees.component";
 
-const routes: Routes = [];
+const {
+  feesManagement: {fees},
+} = Permissions;
+
+const routes: Routes = [
+  {
+    path: 'addfees',
+    canActivate: [AuthGuard],
+    component: AddfeesComponent,
+    data: {
+      permissions: [fees.ADD_FEES],
+    },
+  },
+  {
+    path: 'allfees',
+    canActivate: [AuthGuard],
+    component: AllfeesComponent,
+    data: {
+      permissions: [fees.VIEW_FEES],
+    },
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
+
+
 export class FeesRoutingModule { }
