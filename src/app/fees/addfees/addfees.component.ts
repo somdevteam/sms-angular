f2d1a3e45a6e8bb67a41b53497d150bab760cba3
Abdihakiim121 <cabdixakiin121@gmail.com>
2024-07-25 06:18:32 +0300

    improvement

--- src/app/fees/addfees/addfees.component.ts
+++ src/app/fees/addfees/addfees.component.ts
@@ -51,23 +51,23 @@ export class AddfeesComponent {
     this.loadPaymentTypes();
 
   }
-  onChangeRollNumber(value: any) {
-    this.selectedRollNumber = Number(value.target.value);
-    // const fees =this.feesServices.getStudentByRollNumber(this.selectedRollNumber);
-    // console.log(fees);
-
-    this.feesServices.getStudentByRollNumber(this.selectedRollNumber).subscribe({
-      next: (res) => {
-        if (res.length > 0) {
-          console.log(res);
-        }
-      },
-      error: (error) => {
-        console.log(error);
-        this.snackBar.dangerNotification(error);
-      },
-    });
-  }
+  // onChangeRollNumber(value: any) {
+  //   this.selectedRollNumber = Number(value.target.value);
+  //   // const fees =this.feesServices.getStudentByRollNumber(this.selectedRollNumber);
+  //   // console.log(fees);
+  //
+  //   this.feesServices.getStudentByRollNumber(this.selectedRollNumber).subscribe({
+  //     next: (res) => {
+  //       if (res.length > 0) {
+  //         console.log(res);
+  //       }
+  //     },
+  //     error: (error) => {
+  //       console.log(error);
+  //       this.snackBar.dangerNotification(error);
+  //     },
+  //   });
+  // }
 
   loadPaymentTypes() {
     this.feesServices.getPaymentTypes().subscribe({
@@ -80,6 +80,30 @@ export class AddfeesComponent {
       })
     })
   }
+
+  onChangeRollNumber(event: any) {
+    this.selectedRollNumber = Number(event.target.value);
+    this.feesServices.getStudentByRollNumber(this.selectedRollNumber).subscribe({
+      next: (res) => {
+        if (res.data) {
+          const studentData = res.data;
+          const studentName = `${studentData.firstname} ${studentData.middlename} ${studentData.lastname}`;
+          const studentClassId = studentData.studentClass[0]?.studentClassId;
+
+          // Set the retrieved values into the form
+          this.feesForm.patchValue({
+            sName: studentName
+           // studentClassId: studentClassId
+          });
+        }
+      },
+      error: (error) => {
+        console.log(error);
+        this.snackBar.dangerNotification(error);
+      },
+    });
+  }
+
 
   onSubmit() {
     console.log('Form Value', this.feesForm.value);
